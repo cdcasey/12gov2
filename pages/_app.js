140f82021c747aac0d4eb391b6be6d84f82ad8bd
Chris Casey <cdcasey@gmail.com>
2021-04-17 00:17:53 -0500

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/index.css'
+import '@fontsource/cabin'
+import '@fontsource/lilita-one'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
