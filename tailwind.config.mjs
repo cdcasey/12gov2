ad571029ef169c339f18d1b4bd35e859a6c941df
Chris Casey <cdcasey@users.noreply.github.com>
2024-12-28 21:16:05 -0600

    WIP

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				otgogreen: {
+					light: '#b4d55f',
+					medium: '#4c9342',
+					dark: '#3f421b',
+				},
+				otgopurple: {
+					light: '#692f65',
+					dark: '#4e2688',
+				},
+			}
+		},
 	},
 	plugins: [],
 }
